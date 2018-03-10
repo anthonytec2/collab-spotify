a0e9d4c1f432ca8838692863885fc90875762382
Anthony Bisulco <bisulco.a@husky.neu.edu>
2018-03-10 08:51:36 -0500

    added controller for progam control

--- run_display.py
+++ run_display.py
@@ -3,37 +3,44 @@ from tabulate import tabulate
 import os
 import spotify
 import sys
-clear = lambda: os.system('clear')
+
+
+def clear(): return os.system('clear')
+
 
 class run_display:
-    def __init__(self,sp):
+    def __init__(self, sp):
         self.logger = logging.getLogger(__name__)
-        self.sp=sp
 
-    def prompt_song(self):
+    def prompt(self, desc='Song to Search: '):
+        """[Prompts user to put in song name]
+
+        Returns:
+            [string] -- [song name]
+        """
         try:
-            song_name = input('Song to Search: ').strip()
+            song_name = input(desc).strip()
             return song_name
         except:
             self.logger.debug("Error trying to parse song name")
             return -1
-    
+
     def select_number(self, low=0, high=10):
         """[Number selector from user options on screen]
-        
+
         Keyword Arguments:
             low {int} -- [lowest int to input] (default: {0})
             high {int} -- [highest int to input] (default: {10})
-        
+
         Raises:
             ValueError -- [if try to input any val except int]
-        
+
         Returns:
             [int] -- [number of selection]
         """
         try:
             option = int(input('Option: ').strip())
-            if low<=option<=high:
+            if low <= option <= high:
                 return option
             else:
                 raise ValueError
@@ -41,24 +48,25 @@ class run_display:
             self.logger.debug("Error trying to parse options #")
             return -1
 
-    def display_user_playlists(self):
-        """[displays all the songs from a users playlist]
+    def display_user_playlists(self, pl_names, pl_ids):
+        """[displays all the playlists from a users playlist]
         """
-        pl_names,_ = self.sp.list_playlists()
-        pl_names_ls=[]
-        pl_names=[[i,pl_name] for i,pl_name in enumerate(pl_names)]
+        pl_names_ls = []
+        pl_names = [[i, pl_name] for i, pl_name in enumerate(pl_names)]
         clear()
         print(tabulate(pl_names))
+        return pl_ids, len(pl_names)
 
     def display_user_tracks(self, track_ls):
         """[displays all the users tracks]
-        
+
         Arguments:
             track_ls {[list]} -- [list of lists of song name, album, time and popularity]
         """
 
         clear()
-        print(tabulate(track_ls, headers=["#", "Name","Album","Time","Popularity"]))
+        print(tabulate(track_ls, headers=[
+              "#", "Name", "Album", "Time", "Popularity"]))
 
     def display_commands(self):
         """[displats all commands for user]
@@ -69,3 +77,5 @@ class run_display:
         print("2) Add new song to playlist")
         print("3) List Playlists")
         print("4) Print Songs in Playlist")
+        print("5) Create Playlist")
+        print("6) Exit")
--- spotify.py
+++ spotify.py
@@ -20,8 +20,7 @@ class spotify:
         Arguments:
             username {[string]} -- [username of spotify user]
         """
-        self.user = username
-        self.sp = self.init_auth_client()
+        self.sp, self.user = self.init_auth_client()
         self.logger = logging.getLogger(__name__)
 
     def init_auth_client(self):
@@ -29,14 +28,14 @@ class spotify:
 
         Returns:
             [Spotify] -- [spotify authorized client]
+            [String]  -- [username]
         """
         with open("config.yml", 'r') as ymlfile:
             cfg = yaml.load(ymlfile)
-        print(cfg)
-        token = util.prompt_for_user_token(self.user, scope=cfg['scope'], client_id=cfg['spotipy_client_id'],
+        token = util.prompt_for_user_token(cfg['username'], scope=cfg['scope'], client_id=cfg['spotipy_client_id'],
                                            client_secret=cfg['spotipy_client_secret'], redirect_uri=cfg['spotipy_redirect_uri'])
         sp = spotipy.Spotify(auth=token)
-        return sp
+        return sp, cfg['username']
 
     def create_new_playlist(self, name, desc=''):
         """[creates a new playlist with given name, desc with given limts]
@@ -80,19 +79,22 @@ class spotify:
             i = i+1
         return songs_ls, table_ls
 
-    def list_playlists(self):
+    def list_playlists(self, user=None):
         """[list all spotify playlists on users account]
 
         Returns:
             pl_names [string] -- [lists of playlist names]
             pl_id [string] -- [list of playlist ids]
         """
-        playlists = self.sp.user_playlists(self.user)['items']
+        if user:
+            playlists = self.sp.user_playlists(user)['items']
+        else:
+            playlists = self.sp.user_playlists(self.user)['items']
         pl_names = [pl['name'] for pl in playlists]
         pl_id = [pl['id'] for pl in playlists]
         return pl_names, pl_id
 
-    def list_pl_songs(self, pl_id):
+    def list_pl_songs(self, pl_id, user=None):
         """[list all the songs for a given playlist id]
 
         Arguments:
@@ -101,11 +103,18 @@ class spotify:
         Returns:
             [string] -- [list of song uris in playlist]
         """
-        res = self.sp.user_playlist_tracks(self.user, pl_id)
+        if user:
+            res = self.sp.user_playlist_tracks(user, pl_id)
+        else:
+            res = self.sp.user_playlist_tracks(self.user, pl_id)
         song_uri_ls = [song['track']['uri'] for song in res['items']]
-        return song_uri_ls
+        song_ls = []
+        for i, song in enumerate(res['items']):
+            song_ls.append([i, song['track']['name'][0:20].strip(), song['track']['album']['name'][0:20].strip(
+            ), "%0.2f" % (song['track']['duration_ms']/60000), song['track']['popularity']])
+        return song_uri_ls, song_ls
 
-    def add_song_to_playlist(self, song_uri, playlist_id):
+    def add_song_to_playlist(self, song_uri, playlist_id, user=None):
         """[adds a song to a playlist]
 
         Arguments:
@@ -113,7 +122,11 @@ class spotify:
             playlist_id {[string]} -- [playlist id to add song]
         """
 
-        if song_uri[0] in self.list_pl_songs(playlist_id):
+        if song_uri[0] in self.list_pl_songs(playlist_id, user=None):
             logging.debug('Song already in playlist')
         else:
-            self.sp.user_playlist_add_tracks(self.user, playlist_id, song_uri)
+            if user:
+                self.sp.user_playlist_add_tracks(user, playlist_id, song_uri)
+            else:
+                self.sp.user_playlist_add_tracks(
+                    self.user, playlist_id, song_uri)
